cc43a3edeb5463dbc41e04e7ea08387e7c4f3505
Jason Risch <jason.risch@chloeandisabel.com>
2016-08-31 11:00:37 -0300

    Return more detail on active record invalid exceptions

--- app/controllers/hangar/resources_controller.rb
+++ app/controllers/hangar/resources_controller.rb
@@ -1,8 +1,8 @@
 module Hangar
   class ResourcesController < ActionController::Base
 
-    resque_from ActiveRecord::RecordInvalid, with: :invalid_render_method
     rescue_from StandardError, with: :error_render_method
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_render_method
     def create
       created = FactoryGirl.create resource, *traits, resource_attributes
       render json: created.as_json(include: includes)
@@ -34,8 +34,10 @@ module Hangar
     def error_render_method(exception)
       status_code = ActionDispatch::ExceptionWrapper.new(env, exception).status_code
       respond_to do |type|
-        type.json { render :json => {error: exception.message}.to_json, :status => status_code}
-        type.all { throw exception }
+        type.all {
+          error = {message: exception.message, stack_trace: exception.backtrace}
+          render json: error, status: status_code
+        }
       end
       true
     end
@@ -43,7 +45,10 @@ module Hangar
     def invalid_render_method(exception)
       status_code = ActionDispatch::ExceptionWrapper.new(env, exception).status_code
       respond_to do |type|
-        type.all { render :json => {error: exception.message, object: exception.record.to_json}.to_json, :status => status_code }
+        type.all {
+          error = {message: exception.message, record: exception.record}.to_json(include: includes)
+          render json: error, status: status_code
+        }
       end
       true
     end
