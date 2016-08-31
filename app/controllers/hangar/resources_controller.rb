f83551159f8d607dd0f4e537e614fd014e994bdc
Jason Risch <jason.risch@chloeandisabel.com>
2016-08-31 10:21:04 -0300

    Return more detail on active record invalid exceptions

--- app/controllers/hangar/resources_controller.rb
+++ app/controllers/hangar/resources_controller.rb
@@ -1,8 +1,8 @@
 module Hangar
   class ResourcesController < ActionController::Base
 
-    rescue_from StandardError, :with => :error_render_method
-
+    resque_from ActiveRecord::RecordInvalid, with: :invalid_render_method
+    rescue_from StandardError, with: :error_render_method
     def create
       created = FactoryGirl.create resource, *traits, resource_attributes
       render json: created.as_json(include: includes)
@@ -36,6 +36,14 @@ module Hangar
       respond_to do |type|
         type.json { render :json => {error: exception.message}.to_json, :status => status_code}
         type.all { throw exception }
+      end
+      true
+    end
+
+    def invalid_render_method(exception)
+      status_code = ActionDispatch::ExceptionWrapper.new(env, exception).status_code
+      respond_to do |type|
+        type.all { render :json => {error: exception.message, object: exception.record.to_json}.to_json, :status => status_code }
       end
       true
     end
